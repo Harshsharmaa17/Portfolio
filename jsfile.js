6be4a67597a17fb07f3ca6ec71b58c9de5ea2c28
Harsh Sharma <110018503+Harshsharmaa17@users.noreply.github.com>
2022-12-02 23:25:31 +0530

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,42 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+setInterval(showTime, 1000);
+        function showTime() {
+            let time = new Date();
+            let hour = time.getHours();
+            let min = time.getMinutes();
+            let sec = time.getSeconds();
+            am_pm = " PM";
+ 
+            if (hour > 12) {
+                hour -= 12;
+                am_pm = " AM";
+            }
+            if (hour == 0) {
+                hr = 12;
+                am_pm = " PM";
+            }
+ 
+            hour = hour < 10 ? "0" + hour : hour;
+            min = min < 10 ? "0" + min : min;
+            sec = sec < 10 ? "0" + sec : sec;
+ 
+            let currentTime = hour + ":"
+                + min + ":" + sec + am_pm;
+ 
+            document.getElementById("clock")
+                .innerHTML = currentTime;
+        }
+ 
+        showTime();
+       
